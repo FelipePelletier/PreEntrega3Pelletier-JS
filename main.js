c3a153144e677c02dd1cb1666f93f250ec6d49ef
Felipe Pelletier <felipelletier@gmail.com>
2023-07-31 00:21:30 -0300

    agrego alerts de libreria y API

--- main.js
+++ main.js
@@ -106,11 +106,90 @@ const borrarTablaDesaprobados = () => {
 };
 
 document.getElementById("formulario").addEventListener("submit", agregarAlumno);
+
 document
   .getElementById("btnBorrarAprobados")
   .addEventListener("click", borrarTablaAprobados);
+
 document
   .getElementById("btnBorrarDesaprobados")
   .addEventListener("click", borrarTablaDesaprobados);
 
 document.addEventListener("DOMContentLoaded", cargarAlumnosDesdeLocalStorage);
+
+const getAlumnosByApi = async () => {
+  try {
+    let response = await fetch(
+      "https://64c41a7467cfdca3b660a028.mockapi.io/api/alumnos"
+    );
+
+    return response.json();
+  } catch (error) {
+    console.log("error en api alumnos");
+  }
+};
+
+getAlumnosByApi().then((data) =>
+  data.forEach((alumno) =>
+    alumnos.push({
+      ...alumno,
+      nombre: alumno.name.split(" ")[0],
+      apellido: alumno.name.split(" ")[1],
+      nota1: redondearNota(alumno.nota1),
+      nota2: redondearNota(alumno.nota2),
+    })
+  )
+);
+
+const redondearNota = (nota) => {
+  if (nota <= 10) return 1;
+  if (nota <= 20) return 2;
+  if (nota <= 30) return 3;
+  if (nota <= 40) return 4;
+  if (nota <= 50) return 5;
+  if (nota <= 60) return 6;
+  if (nota <= 70) return 7;
+  if (nota <= 80) return 8;
+  if (nota <= 90) return 9;
+  if (nota <= 100) return 10;
+};
+
+document
+  .getElementById("cargarAlumnosAPI")
+  .addEventListener("click", mostrarAlumnos);
+
+const alumnoConfirmado = document.querySelector("#alumnoConfirmado");
+
+alumnoConfirmado.addEventListener("click", () => {
+  Swal.fire({
+    position: "top-end",
+    icon: "success",
+    title: "Alumno agregado!",
+    showConfirmButton: false,
+    timer: 1500,
+  });
+});
+
+const alertTablaAP = document.querySelector("#btnBorrarAprobados");
+
+alertTablaAP.addEventListener("click", () => {
+  Swal.fire({
+    position: "top-end",
+    icon: "success",
+    title: "Tabla aprobados borrada!",
+    showConfirmButton: false,
+    timer: 1500,
+  });
+});
+
+const alertTablaDP = document.querySelector("#btnBorrarDesaprobados");
+
+alertTablaDP.addEventListener("click", () => {
+  Swal.fire({
+    position: "top-end",
+    icon: "success",
+    title: "Tabla desaprobados borrada!",
+    showConfirmButton: false,
+    timer: 1500,
+  });
+});
